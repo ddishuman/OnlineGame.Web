c6bbb49eae57704db7434efd74f153416ccebbe0
Martin Wang <alpa.wong@gmail.com>
2018-12-16 21:15:25 +0800

    CheckBox practice end (tutorial 13)

--- OnlineGame.Web/App_Start/RouteConfig.cs
+++ OnlineGame.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace OnlineGame.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Gamer", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- OnlineGame.Web/Controllers/GamerController.cs
+++ OnlineGame.Web/Controllers/GamerController.cs
@@ -1,14 +1,61 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using OnlineGame.Web.Models;
+using PagedList;
+
 namespace OnlineGame.Web.Controllers
 {
     public class GamerController : Controller
     {
+        private OnlineGameEntities db = new OnlineGameEntities();
+
         [HttpGet]
-        public ActionResult Index()
+        public async Task<ActionResult> Index(string searchBy, string searchText, int? pageNumber, string sortBy)
         {
-            return View();
+            ViewBag.NameSort = string.IsNullOrEmpty(sortBy) ? "Name desc" : "";
+            ViewBag.GenderSort = sortBy == "Gender" ? "Gender desc" : "Gender";
+
+            List<Gamer> gamers = await db.Gamers.ToListAsync();
+            if (searchBy == "Gender")
+            {
+                gamers = await db.Gamers
+                    .Where(x => x.Gender == searchText || searchText == null)
+                    .ToListAsync();
+            }
+            if (searchBy == "Name")
+            {
+                gamers = await db.Gamers
+                    .Where(x => x.Name.Contains(searchText) || searchText == null)
+                    .ToListAsync();
+            }
+
+            IOrderedEnumerable<Gamer> gamersOrderedEnumerable;
+            switch (sortBy)
+            {
+                case "Name desc":
+                    gamersOrderedEnumerable = gamers.OrderByDescending(x => x.Name);
+                    break;
+                case "Gender desc":
+                    gamersOrderedEnumerable = gamers.OrderByDescending(x => x.Gender);
+                    break;
+                case "Gender":
+                    gamersOrderedEnumerable = gamers.OrderBy(x => x.Gender);
+                    break;
+                default:
+                    gamersOrderedEnumerable = gamers.OrderBy(x => x.Name);
+                    break;
+            }
+
+
+            //IPagedList<Gamer> gamerPagedList = gamers.ToPagedList(pageNumber ?? 1, 5);
+            IPagedList<Gamer> gamerPagedList = gamersOrderedEnumerable.ToPagedList(pageNumber ?? 1, 5);
+            return View(gamerPagedList);
         }
+
+
         [HttpGet]
         public ActionResult Index2()
         {
@@ -55,6 +102,16 @@ namespace OnlineGame.Web.Controllers
                 Score = 2000
             };
             return PartialView("_GamerDetails", gamer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteMultiple(IEnumerable<int> GamerIdsToDelete, 
+            string searchBy, string searchText, int? pageNumber, string sortBy)
+        {
+            List<Gamer> gamers = await db.Gamers.Where(g => GamerIdsToDelete.Contains(g.Id)).ToListAsync();
+            gamers.ForEach(g => db.Gamers.Remove(g));
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", new { searchBy, searchText, pageNumber, sortBy });
         }
     }
 }
